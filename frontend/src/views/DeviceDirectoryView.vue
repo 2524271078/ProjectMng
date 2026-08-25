d478b4aa69c12fdcfda848e9cf4a01e0a1703223
2524271078 <2524271078@qq.com>
2026-08-25 15:14:06 +0800

    隐藏设备中心列表前两列

--- frontend/src/views/DeviceDirectoryView.vue
+++ frontend/src/views/DeviceDirectoryView.vue
@@ -91,8 +91,6 @@
 
     <div class="page-table-scroll">
       <el-table v-loading="devicePagination.loading" :data="devicePagination.rows" stripe height="100%">
-        <el-table-column prop="name" label="产品型号" min-width="180" />
-        <el-table-column prop="serial_number" label="序列号" min-width="160" />
         <el-table-column label="设备名称" min-width="180" show-overflow-tooltip>
           <template #default="scope">{{ scope.row.device_model_detail?.model_name || '-' }}</template>
         </el-table-column>
